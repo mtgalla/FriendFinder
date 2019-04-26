26d7c7c05a8ae7b0e996b0cd7bc96e00d6f0827d
mtgalla <mtgalla@gmail.com>
2019-04-25 21:03:59 -0500

    update html links

--- server.js
+++ server.js
@@ -7,10 +7,12 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static("."));
+app.use(express.static("public"));
 //app.use("/code", express.static(__dirname + "/code"));
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
+
 
 
 app.listen(PORT, function(){
