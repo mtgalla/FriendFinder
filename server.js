a7aa3db47970985a76c93834456417c7de910672
mtgalla <mtgalla@gmail.com>
2019-04-27 22:50:59 -0500

    fix css path

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+//middleware that helps encode the object data and 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(express.static("."));
-app.use(express.static("public"));
+//app.use(express.static("."));
+//app.use(express.static("public"));
+app.use(express.static(path.join(__dirname + '/app')));
 //app.use("/code", express.static(__dirname + "/code"));
 
 require("./app/routing/apiRoutes")(app);
